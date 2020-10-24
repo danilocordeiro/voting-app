cfcc271770b18375f53ee45e31e7934c1b28ea5b
Danilo <danilocordeiro.ti@gmail.com>
2020-10-24 00:48:56 -0300

    Adicionado users module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [GraphQLModule.forRoot({
       autoSchemaFile: true,
       playground: true,
       introspection: true
-    }),
+    }), UsersModule,
   ],
 })
 export class AppModule {}
