302bd8839c61d4d6e8f9bc7ca69ec77b4af2458f
Danilo <danilocordeiro.ti@gmail.com>
2020-10-31 01:44:50 -0300

    Add dotenv

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,12 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
+  console.log(process.env.SENDGRID_API_KEY);
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
